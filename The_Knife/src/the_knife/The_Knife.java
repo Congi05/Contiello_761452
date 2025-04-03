20022cfaf961d4ffd407c2683d5e30850c58aeca
Congi05 <contiello.gianni05@gmail.com>
2025-04-03 12:41:13 +0200

    Initial commit

---
+++ The_Knife/src/the_knife/The_Knife.java
@@ -0,0 +1,12 @@
+package the_knife;
+
+public class The_Knife {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        GUI.main(args);
+    }
+    
+}
